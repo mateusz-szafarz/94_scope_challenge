2368d02a8ef36c3bd48e128e2dc08cb50bd99018
Mateusz Szafarz <mateuszszafarz@gmail.com>
2018-01-23 21:14:29 +0000

    Initial implementation

--- src/com/mateuszszafarz/Main.java
+++ src/com/mateuszszafarz/Main.java
@@ -1,8 +1,33 @@
 package com.mateuszszafarz;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner x = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        int x;
+        x();
+        do {
+            x = Main.x.nextInt();
+            Main.x.nextLine(); // clear input
+            switch (x) {
+                case 0:
+                    break;
+                case 1:
+                    x();
+                    break;
+                default:
+                    (new X()).x(x); // print times table
+            }
+        } while (x != 0);
+    }
+
+    private static void x() {
+        System.out.println("Menu:");
+        System.out.println("\t [0] - Quit program.");
+        System.out.println("\t [1] - Print this menu.");
+        System.out.println("\t [any number] - Print times table for that number.\n");
     }
 }
---
+++ src/com/mateuszszafarz/X.java
@@ -0,0 +1,11 @@
+package com.mateuszszafarz;
+
+public class X {
+    private int x;
+    public void x(int x) {
+        for(this.x = 1; this.x <= 10; this.x++) {
+            System.out.println(String.format("%d times %d = %d", this.x, x, this.x * x));
+        }
+        System.out.println();
+    }
+}
